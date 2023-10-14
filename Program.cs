45a70976b95163075527d11e6bc2e0cc99b8ecde
Adam Gheczi <adamark03gmail.com>
2023-10-15 00:13:22 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Vigenere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VigenereCipher vigenere = new VigenereCipher();
+
+            string text = "This is some sample text";
+            string key = "vigenere";
+
+            string encrypted = vigenere.Encrypt(text, key);
+            string decrypted = vigenere.Decrypt(encrypted, key);
+
+            Console.WriteLine($"Text: {text}");
+            Console.WriteLine($"Key: {key}");
+            Console.WriteLine($"Encrypted: {encrypted}");
+            Console.WriteLine($"Decrypted: {decrypted}");
+        }
+    }
+}
---
+++ VigenereCipher.cs
@@ -0,0 +1,22 @@
+﻿namespace Vigenere
+{
+    internal class VigenereCipher
+    {
+        private string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        public string Encrypt(string plainText, string key) => ShiftText(plainText, key, forward: true);
+        public string Decrypt(string plainText, string key) => ShiftText(plainText, key, forward: false);
+
+        private string ShiftText(string plainText, string key, bool forward) =>
+            new string(plainText.Select((c, i) => Char.IsLetter(c) ? ShiftCharacter(c, key[i % key.Length], forward) : c).ToArray());
+
+        private char ShiftCharacter(char baseChar, char shiftChar, bool forward)
+        {
+            string casedAlphabet = char.IsUpper(baseChar) ? alphabet.ToUpper() : alphabet;
+
+            int shiftAmount = forward ? casedAlphabet.IndexOf(shiftChar) : -casedAlphabet.IndexOf(shiftChar);
+
+            return casedAlphabet[(casedAlphabet.IndexOf(baseChar) + shiftAmount + casedAlphabet.Length) % casedAlphabet.Length];
+        }
+    }
+}
